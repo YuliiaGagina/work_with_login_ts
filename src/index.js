d3dfe0531b32e1f50781ff8cda27cca75c82aaf2
YuliiaGagina <ygaginazy@gmail.com>
2023-03-03 23:01:29 +0200

    add pages

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter   basename='/goit-react-hw-08-phonebook/'  >
-  
     <App/>
     </BrowserRouter>
     </PersistGate>
